d052448aa32a06a78c6272228ead1d52e3f559b8
Neeal Singroha <iamneealsingroha@gmail.com>
2026-01-11 11:11:19 +0530

    Update neealQuiz.py

--- neealQuiz.py
+++ neealQuiz.py
@@ -1,1 +1,37 @@
-
+print("This is a quiz about Neeal.")
+print("You will get a score at the end based on your answers.")
+print("Please answer the following questions:")
+a=0
+print("1. What is Neeal's current email address?")
+print("a) iamneealsingroha@gmail.com")
+print("b) neealsingroha@gmail.com")
+print("c) neeal.singroha@gmail.com")
+print("d)maishaneeal@gmail.com")
+z=input("Enter your answer here, only the letter: ")
+if z=="a" or z=="A":
+    print("Correct!")
+    a=a+1
+else:
+    print("Incorrect. The correct answer is a)iamneealsingroha@gmail.com")
+print("2. What is Neeal's favorite color?")
+print("a) Yellow")
+print("b) Red")
+print("c) Blue")
+print("d) Green")
+y=input("Enter your answer here, only the letter: ")
+if y=="c" or y=="C":
+    print("Correct!")
+    a=a+1
+else:
+    print("Incorrect. The correct answer is c)Blue")
+print("3. What is Neeal's favorite hobby?")
+print("a) Reading")
+print("b) Playing video games")
+print("c) Coding")
+print("d) Watching TV")
+x=input("Enter your answer here, only the letter: ")
+if x=="a" or x=="A":
+    print("Correct!")
+    a=a+1
+else:
+    print("Incorrect. The correct answer is a)Reading")
